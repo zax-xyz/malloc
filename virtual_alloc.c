68ed1141ce5689fad77241d2e86f8b61b2de7933
Michael Vo <zax@zaxu.xyz>
2021-04-25 15:42:55 +1000

    monkaHmm

--- virtual_alloc.c
+++ virtual_alloc.c
@@ -183,8 +183,9 @@ void virtual_info(void* heapstart) {
     for (block_t* block = (block_t*) ((uint8_t*) heapstart + (1 << heap_size));
             (uint8_t*) block < prog_break - 2;
             block++) {
-        printf("%s %d\n",
+        printf("%s %d %s\n",
                 block->allocated ? "allocated" : "free",
-                1 << block->size);
+                1 << block->size,
+                block->right ? "right": "left");
     }
 }
